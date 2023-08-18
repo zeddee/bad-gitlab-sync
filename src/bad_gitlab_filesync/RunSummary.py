49581248232c30f27d9872497ac5cc9ade695b92
zed tan <zed@shootbird.work>
2023-08-18 17:40:32 +0200

    Add RunSummary class. Improve error handling. Now prints summary at the end of run

---
+++ src/bad_gitlab_filesync/RunSummary.py
@@ -0,0 +1,12 @@
+
+class RunSummary():
+    def __init__(self, success: int=0, warnings: int=0, errors: int=0):
+        """Simple object to store number of occurrences
+        for these log levels: logging.info, logging.warning, logging.error
+        """
+        self.success = success
+        self.warnings = warnings
+        self.errors = errors
+
+    def __str__(self) -> str:
+        return f"RunSummary(success:{self.success}, warnings:{self.warnings}, errors:{self.errors})"
