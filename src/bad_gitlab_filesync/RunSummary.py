3194fed36c90ae538f0802714e81b02bc5188459
zed tan <zed@shootbird.work>
2023-08-18 17:42:01 +0200

    Fix typo in comment: s/SUMMARY.info/SUMMARY.success/

--- src/bad_gitlab_filesync/RunSummary.py
+++ src/bad_gitlab_filesync/RunSummary.py
@@ -2,7 +2,7 @@
 class RunSummary():
     def __init__(self, success: int=0, warnings: int=0, errors: int=0):
         """Simple object to store number of occurrences
-        for these log levels: logging.info, logging.warning, logging.error
+        for these log levels: logging.success, logging.warning, logging.error
         """
         self.success = success
         self.warnings = warnings
